85e5e113417a3cebdd0388ff1aa7fb0ab21c5f75
Dan Choiniere <dchoinie@gmail.com>
2019-02-22 15:16:35 -0600

    Commit 2/22/19

--- app/router.js
+++ app/router.js
@@ -7,6 +7,28 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('login');
+  this.route('register');
+  this.route('dashboard');
+  this.route('rates');
+  this.route('single-day-passes');
+  this.route('tanning');
+  this.route('massage');
+  this.route('personal-training');
+  this.route('hours');
+  this.route('programs');
+  this.route('membership-options');
+  this.route('equipment');
+  this.route('shop');
+  this.route('yoga');
+  this.route('silver-sneakers');
+  this.route('reimbursement');
+  this.route('about');
+  this.route('privacy-policy');
+  this.route('terms-of-use');
+  this.route('club-policies');
+  this.route('accessibility-policy');
+  this.route('contact');
 });
 
 export default Router;
