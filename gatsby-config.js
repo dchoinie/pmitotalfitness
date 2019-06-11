8284cd5b60227b616c5e2fbf7baa0a44f23bec9d
Dan Choiniere <dchoinie@gmail.com>
2019-06-11 12:51:32 -0500

    Added favicon & gatsby-plugin-favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
